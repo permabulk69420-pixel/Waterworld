95739128efdd4a3f995cd62ceaa542a0cd9cbfc8
permabulk69420-pixel <permabulk69420@gmail.com>
2026-07-31 06:54:58 +1000

    Tag colossus orientation fix

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ import { VRHands } from './player/VRHands.ts';
 import { BootSettings } from './ui/BootSettings.ts';
 import { installShipCollision } from './world/ShipCollisionSystem.ts';
 
-const BUILD_TAG = 'BUILD-MODE-V21-60M-COLOSSUS';
+const BUILD_TAG = 'BUILD-MODE-V22-COLOSSUS-UPRIGHT';
 
 /**
  * Bootstrap.
